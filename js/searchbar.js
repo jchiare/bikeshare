73f65ba4184c55bb6151165af0c53cec7dfb578d
Jay Chiarella <jay.gch93@gmail.com>
2018-04-06 21:52:07 -0400

    Added input & enter eventlisteners for search box

--- js/searchbar.js
+++ js/searchbar.js
@@ -1,17 +1,26 @@
+var user_input;
 
 var search_station_area = document.querySelector('#input');
+search_station_area.addEventListener('input',SearchStationArea);
 search_station_area.addEventListener('keypress',SearchStationArea);
 
-function SearchStationArea(event){
-    var user_input = this.value.toLowerCase();
 
-    if (event.key === 'Enter') {
+function SearchStationArea(event){
+
+    // if user press 'enter', search for the station
+    if (event.type === 'keypress' && event.key === 'Enter'){
         station_names.forEach(function(id,name){
             if (name.toLowerCase().includes(user_input)){
                 console.log(name + ' ' + id);
             }
         })
     }
+    // otherwise, keep appending value to 
+    else if (event.type === 'input'){
+        user_input = this.value.toLowerCase();
+        console.log(this.value);
+    }
+    
 }
 /*
 addStationUI(station_name, docks_available_num, bikes_available_num, time_last_updated) {
