b319ff69bc11d78240ceabf1889cf2df26332684
Jay Chiarella <jay.gch93@gmail.com>
2018-04-06 23:29:18 -0400

    JS works, CSS isn't the best though .. #TODO

--- js/createstationUI.js
+++ js/createstationUI.js
@@ -1,5 +1,22 @@
+var place_invididual_station_ui = document.querySelector('#individual-station-data');
 
+function CreateIndividualStationDiv(){
+    var invidual_station_UI = document.createElement('div');
+    invidual_station_UI.classList.add('individual-station');
+    return invidual_station_UI;
+}
 
+function AddStationData(HTML_element,data_value,data_name,parent_element){
+    var station_data_item = document.createElement(HTML_element);
+    station_data_item.innerHTML = ('<strong> ' + data_name + '</strong> ' + data_value)
+    parent_element.appendChild(station_data_item);
+}
 
 function CreateStationUI(station_name,docks_available,bikes_available,time_last_updated){
+    var this_station = CreateIndividualStationDiv();
+    AddStationData('h2',station_name,'',this_station); // add station name
+    AddStationData('p',docks_available,'Docks available:',this_station); // add dock amounts
+    AddStationData('p',bikes_available,'Bikes available:',this_station); // add bike amount
+    AddStationData('p',time_last_updated,'Last Updated:',this_station); // add last time updated
+    place_invididual_station_ui.appendChild(this_station);
 }
--- js/searchbar.js
+++ js/searchbar.js
@@ -24,27 +24,3 @@ function SearchStationArea(event){
     }
     
 }
-/*
-addStationUI(station_name, docks_available_num, bikes_available_num, time_last_updated) {
-    const div = document.createElement('div');
-    div.classList.add('bikeshare');
-
-    const heading = document.createElement('h2');
-    heading.textContent = `${station_name}`;
-
-    const docks = document.createElement('p');
-    docks.innerHTML = `<strong>Docks available:</strong> ${docks_available_num}`;
-
-    const bikes = document.createElement('p');
-    bikes.innerHTML = `<strong>Bikes available:</strong> ${bikes_available_num}`;
-
-    const time = document.createElement('p');
-    time.innerHTML = `<strong>Last Updated:</strong> ${time_last_updated} ago`;
-
-    // append errthang
-    div.appendChild(heading);
-    div.appendChild(docks);
-    div.appendChild(bikes);
-    div.appendChild(time);
-    find_container.appendChild(div);
-  },*/
