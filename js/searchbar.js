9b0862692ecea8a7bba2bb7833e7b116c2af51ac
Jay Chiarella <jay.gch93@gmail.com>
2018-04-07 13:11:44 -0400

    hooblah

--- js/searchbar.js
+++ js/searchbar.js
@@ -3,19 +3,22 @@ search_station_area.addEventListener('input',SearchStationArea);
 
 function SearchStationArea(event){
     
-    // clear individual station UIs for each event -- to get freshest data
+    // clear individual station UIs for each event -- to display freshest data according to search field
     while (place_invididual_station_ui.hasChildNodes()){
         place_invididual_station_ui.removeChild(place_invididual_station_ui.lastChild);
     }
-
-    // get value from search field, and search stations via the name 
-    var user_input = this.value.toLowerCase();
-    station_names.forEach(function(id,name){
-        if (name.toLowerCase().includes(user_input)){
-            CreateStationUI(name,
-                            docks_available_amount.get(id),
-                            bikes_available_amount.get(id),
-                            station_last_report_timestamp.get(id));
-        }
-    })
+    
+    if (this.value !== ""){
+         // get value from search field, and search stations via the name 
+         var user_input = this.value.toLowerCase();
+         station_names.forEach(function(id,name){
+             if (name.toLowerCase().includes(user_input)){
+                 CreateStationUI(name,
+                                 docks_available_amount.get(id),
+                                 bikes_available_amount.get(id),
+                                 station_last_report_timestamp.get(id));
+             }
+         })   
+    }
+   
 }
