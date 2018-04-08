baef099d31364aec2ff04624e8afac20a2cdaad2
Jay Chiarella <jay.gch93@gmail.com>
2018-04-08 14:51:22 -0400

    final push for geolocation

--- js/geolocation.js
+++ js/geolocation.js
@@ -4,6 +4,10 @@ var current_location_button = document.querySelector('#location');
 var user_lat;
 var user_lon;
 
+var geolocation_options = {
+    enableHighAccuracy: true
+};
+
 function SuccessGeolocationCall(position){
     user_lat = position.coords.latitude;
     user_lon = position.coords.longitude;
@@ -14,7 +18,7 @@ function GeolocationError(error){
 }
 
 function GetLocation(){
-    navigator.geolocation.getCurrentPosition(SuccessGeolocationCall,GeolocationError);
+    navigator.geolocation.getCurrentPosition(SuccessGeolocationCall,GeolocationError,geolocation_options);
 }
 
 current_location_section.addEventListener('click',function(e){
